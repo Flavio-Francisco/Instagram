d04311aed5010abaa309a8bdaf1e90f0f38b52c0
Flavio Francisco <flavyobass151@gmail.com>
2023-05-08 17:04:30 -0300

    adicionando o header

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,13 +1,24 @@
 
 import { StyleSheet, Text, View } from 'react-native';
-
+import Logo from "../../assets/logo.svg"
+import Stroke from "../../assets/stroke.svg"
+import Menssagem from "../../assets/message.svg"
 
 
 
 export  function Home() {
   return (
     <View style={styles.container}>
-     
+     <View style={styles.header}>
+       <Logo/>
+       <View style={styles.headerOption}>
+         <Stroke/> 
+         <Menssagem/>
+       </View>
+     </View>
+     <View>
+
+     </View>
     </View>
   );
 }
@@ -15,8 +26,24 @@ export  function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#0000',
     alignItems: 'center',
   
   },
+  header: {
+    marginTop:40,
+    paddingHorizontal: 10,
+    width:'100%',
+    height:56,
+    alignItems:'center',
+    justifyContent:'space-between',
+    flexDirection:'row'
+   
+  },
+  headerOption:{
+    alignItems:'center',
+    flexDirection:'row',
+    gap:10,
+  }
+
 });
